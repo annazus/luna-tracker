057bf2b50b8b82d71a2361bf66d752db78045301
annazus <suzanna@appliedar.us>
2019-06-19 19:04:59 -0700

    Updated logo and toolbar color

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Layout from "./components/Layout";
 import Signup from "./components/Auth/Signup";
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LogoImage from "../../assets/images/burger-logo.png";
+import LogoImage from "../../assets/images/luna-logo.svg";
 import classes from "./Logo.module.css";
 const Logo = () => (
   <img src={LogoImage} alt="Luna Tracker Logo" className={classes.Logo} />
