ce508c525d2e1e51b1cfca321b1cc8dc70e183cb
annazus <suzanna@appliedar.us>
2019-06-19 19:15:52 -0700

    Added text to logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,6 +2,9 @@ import React from "react";
 import LogoImage from "../../assets/images/luna-logo.svg";
 import classes from "./Logo.module.css";
 const Logo = () => (
-  <img src={LogoImage} alt="Luna Tracker Logo" className={classes.Logo} />
+  <div className={classes.Logo}>
+    <img src={LogoImage} alt="Luna Tracker Logo" />
+    <h3 className={classes.Title}>Luna Tracker </h3>
+  </div>
 );
 export { Logo as default };
